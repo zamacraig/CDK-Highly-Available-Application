d19227cf07aee04834b8a86281edaae8958a6df8
Zama Dhladhla <zamacraig@gmail.com>
2025-03-09 12:14:15 +0200

    Initial commit

---
+++ src/CdkHighlyAvailableApplication/CdkHighlyAvailableApplicationStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkHighlyAvailableApplication
+{
+    public class CdkHighlyAvailableApplicationStack : Stack
+    {
+        internal CdkHighlyAvailableApplicationStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
